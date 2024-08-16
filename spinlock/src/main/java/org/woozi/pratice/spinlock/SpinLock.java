cdab0fcec0de8892058e6a84d38bd3e0dbe8ffc1
woozi <kwj1270@naver.com>
2024-08-17 01:44:11 +0900

    feat: Modify SpinLock logic

--- spinlock/src/main/java/org/woozi/pratice/spinlock/SpinLock.java
+++ spinlock/src/main/java/org/woozi/pratice/spinlock/SpinLock.java
@@ -6,11 +6,8 @@ public class SpinLock {
     private AtomicBoolean lock = new AtomicBoolean(false);
 
     public void lock() {
-        System.out.println("Trying to acquire lock..." + Thread.currentThread().getName());
         while (!lock.compareAndSet(false, true)) {
-            System.out.println("Waiting for lock..." + Thread.currentThread().getName());
         }
-        System.out.println("Lock acquired..." + Thread.currentThread().getName());
     }
 
     public void unlock() {
--- spinlock/src/main/java/org/woozi/pratice/spinlock/SpinlockApplication.java
+++ spinlock/src/main/java/org/woozi/pratice/spinlock/SpinlockApplication.java
@@ -7,15 +7,15 @@ public class SpinlockApplication {
 
         Runnable task = () -> {
             spinLock.lock();
-            System.out.println("Thread " + Thread.currentThread().getName() + " acquired the lock.");
+            System.out.println("Thread " + Thread.currentThread().getName() + " 가 락을 획득했습니다.");
             try {
                 Thread.sleep(10L);
             } catch (InterruptedException e) {
                 throw new RuntimeException();
             } finally {
-                System.out.println("Thread " + Thread.currentThread().getName() + " will release the lock.");
+                System.out.println("Thread " + Thread.currentThread().getName() + " 가 락을 해제합니다.");
                 spinLock.unlock();
-                System.out.println("Thread " + Thread.currentThread().getName() + " released the lock.");
+                System.out.println("Thread " + Thread.currentThread().getName() + " 가 락을 해제했습니다.");
             }
         };
 
